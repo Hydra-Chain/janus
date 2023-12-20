5dd9635472f9ed83dcd22e6791df62a5be69ced1
Ivaylo Petrov <isivaylopetrov@gmail.com>
2023-12-20 11:47:23 +0200

    Don't rebuild on quick-start

--- docker/spin_up.mainnet.sh
+++ docker/spin_up.mainnet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker-compose --env-file ./.env  -f ./quick_start/docker-compose.mainnet.yml up -d --build
+docker-compose --env-file ./.env  -f ./quick_start/docker-compose.mainnet.yml up -d
 # sleep 3 #executing too fast causes some errors
 # docker cp ${GOPATH}/src/github.com/qtumproject/janus/docker/fill_user_account.sh qtum_testchain:.
 # docker exec qtum_mainnet /bin/sh -c ./fill_user_account.sh
